be9ef1075cdbc516657557e5b6cb18ebd06c23cb
Martin Kåberg <martin.kaberg@nordcloud.com>
2023-04-10 20:32:09 +0200

    added modifier

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import logging
 import discord
 import os
 from discord.ext import commands
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 def dice(sides=6):
     logging.debug("A {} sided dice has been selected".format(sides))
@@ -22,24 +24,26 @@ def hand(number_of_dice=1, sides=6):
             total = total + current_dice
     return total
 
+
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.command(name="ob", help="!ob 3t6")
 async def ob(ctx, input: str):
-
-
-    try :
+    try:
+        modifier = 0
         number_of_dice, sides = input.split("t")
+        if len(sides.split("+")) > 1:
+            sides, modifier = sides.split("+")
         if int(sides) < int(2):
             await ctx.send("You think you smart? HACKERMAN!")
             return
         if int(number_of_dice) > 10:
             await ctx.send("Cant do more than 10 dice on this hardware")
             return
-        await ctx.send(hand(int(number_of_dice), int(sides)))
-    except :
+        await ctx.send(hand(int(number_of_dice), int(sides)) + int(modifier))
+    except:
         await ctx.send("Cant break this!")
 
 
